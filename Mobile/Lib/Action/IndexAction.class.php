7d97578bbe0ad03fd1eb092e0ea67ff6e3afc760
ruansheng <825307513@qq.com>
2014-06-24 14:56:36 +0800

    修改

--- Mobile/Lib/Action/IndexAction.class.php
+++ Mobile/Lib/Action/IndexAction.class.php
@@ -5,18 +5,27 @@ class IndexAction extends Action {
 	/*
 	*获取 LinkList接口
 	* /mobile.php?m=Index&a=getLinkList&more=gt&loadid=0
-	*/
+	* /mobile.php?m=Index&a=getLinkList&more=lt&loadid=10
+	* /mobile.php?m=Index&a=getLinkList&more=lt&loadid=10&cid=1
+	*/   
     public function getLinkList(){
-		$map['id']=array($_GET['more'],$_GET['loadid']);
-		$list=D('feed')->where($map)->select();
-		$result['status']=0;
-		$result['data']=$list;
-		$result['message']="解析成功";
+    	
+    	if(!empty($_GET['more'])&&!empty($_GET['loadid'])){
+	    	$cid=empty($_GET['cid'])? 0:$_GET['cid'];
+	    	if($cid!=0){
+	    		$map['cid']=$_GET['cid'];
+	    	}
+	    	$map['id']=array($_GET['more'],$_GET['loadid']);
+			$list=D('feed')->where($map)->select();
+			$result['status']=0;
+			$result['data']=$list;
+			$result['message']="解析成功";
+    	}else{
+    		$result['status']=-1;
+    		$result['data']=array();
+    		$result['message']="参数不全";
+    	}
 		exit(json_encode($result));
     }
 	
-	public function index(){
-		$feed=new FeedModel();
-		$feed->index();
-	}
 }
