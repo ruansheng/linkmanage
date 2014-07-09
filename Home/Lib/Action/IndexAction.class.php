c15d36637386cec7730aab40248f40210a0b55d5
ruansheng <825307513@qq.com>
2014-07-09 23:02:39 +0800

    替换模板

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -15,8 +15,18 @@ class IndexAction extends Action {
 		//按分页来查询 
 		
 		$list = $feed->table($table)->field($field)->where($map)->order('feed.ctime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		foreach($list as $key=>$val){
+			$list[$key]['domain']=parse_url($val['url'])['host'];
+		}
+		/*
+		echo "<pre>";
+		   print_r($list);
+		echo "</pre>";
+		*/
+		
 		$this->assign('list',$list);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
 		$this->display(); // 输出模板
+		
     }
 }
